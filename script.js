04ec91d1009c07be17de1ba9194ba8db21f1046e
rgbhsl <literarymagazineaphelion@gmail.com>
2026-09-21 15:12:38 +0000

    drag

--- script.js
+++ script.js
@@ -16,3 +16,44 @@ var welcomeScreenClose = document.querySelector("#welcomeclose")
 welcomeScreenClose.addEventListener("click", function() {
   closeWindow(welcomeScreen);
 });
+
+dragElement(document.getElementById("window"));  
+
+function dragElement(element) {
+  var initialX = 0;
+  var initialY = 0;
+  var currentX = 0;
+  var currentY = 0;
+
+  if (document.getElementById(element.id + "header")) {
+    document.getElementById(element.id + "header").onmousedown = startDragging;
+  } else {
+    element.onmousedown = startDragging;
+  }
+
+  function startDragging(e) {
+    e = e || window.event;
+    e.preventDefault();
+    initialX = e.clientX;
+    initialY = e.clientY;
+    document.onmouseup = stopDragging;
+    document.onmousemove = dragElement;
+  }
+
+  function dragElement(e) {
+    e = e || window.event;
+    e.preventDefault();
+    currentX = initialX - e.clientX;
+    currentY = initialY - e.clientY;
+    initialX = e.clientX;
+    initialY = e.clientY;
+    // Step 11: Update the element's new position by modifying its `top` and `left` CSS properties.
+    element.style.top = (element.offsetTop - currentY) + "px";
+    element.style.left = (element.offsetLeft - currentX) + "px";
+  }
+
+  function stopDragging() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
