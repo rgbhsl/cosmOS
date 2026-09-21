0c3ecb5612839f34fb28ecfff244ad61dd6dd8d6
rgbhsl <literarymagazineaphelion@gmail.com>
2026-09-21 15:17:15 +0000

    try

--- script.js
+++ script.js
@@ -57,3 +57,24 @@ function dragElement(element) {
     document.onmousemove = null;
   }
 }
+
+var welcomeScreen = document.querySelector("#welcome")
+function closeWindow(element) {
+  element.style.display = "none"
+}
+function openWindow(element) {
+  element.style.display = "flex"
+}
+
+var welcomeScreenClose = document.querySelector("#welcomeclose")
+
+var welcomeScreenOpen = document.querySelector("#welcomeopen")
+
+welcomeScreenClose.addEventListener("click", function() {
+  closeWindow(welcomeScreen);
+});
+
+welcomeScreenOpen.addEventListener("click", function() {
+  openWindow(welcomeScreen);
+});
+
